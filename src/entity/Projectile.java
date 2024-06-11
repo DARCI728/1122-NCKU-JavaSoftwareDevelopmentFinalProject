1fb0b0d55dfb36234834e73d1bf1da5d27a78712
KawakitaTanaka <145382809+KawakitaTanaka@users.noreply.github.com>
2024-06-11 16:33:33 +0800

    Update Projectile.java

--- src/entity/Projectile.java
+++ src/entity/Projectile.java
@@ -29,7 +29,6 @@ public class Projectile extends Entity {
     }
 
     public void update() {
-
         int objIndex = gp.cChecker.checkObject(this, true);
 
         if (objIndex != -1 && hasCheckObj == false) {
@@ -39,7 +38,7 @@ public class Projectile extends Entity {
         }
 
         if (objIndex != -1 && !(initialCol == initialObjCol && initialRow == initialObjRow)) {
-            Entity arrow = new OBJ_Arrow(gp);
+            Entity arrow = new OBJ_Arrow(gp, direction);
             switch (direction) {
                 case "up":
                     arrow.worldX = gp.obj.get(objIndex).worldX;
@@ -61,17 +60,17 @@ public class Projectile extends Entity {
                     arrow.worldY = gp.obj.get(objIndex).worldY;
                     break;
             }
-
             arrow.direction = direction;
             gp.obj.add(arrow);
             gp.player.pickUpArrow(arrow);
             alive = false;
+            gp.player.saveState();
         }
 
         int mobIndex = gp.cChecker.checkEntity(this, gp.mob);
 
         if (mobIndex != -1) {
-            Entity arrow = new OBJ_Arrow(gp);
+            Entity arrow = new OBJ_Arrow(gp, direction);
             arrow.worldX = gp.mob[mobIndex].worldX;
             arrow.worldY = gp.mob[mobIndex].worldY;
             arrow.direction = direction;
@@ -79,13 +78,14 @@ public class Projectile extends Entity {
             gp.player.pickUpArrow(arrow);
             gp.player.damageMonster(mobIndex);
             alive = false;
+            gp.player.saveState();
         }
 
         int coordinate[] = new int[2];
         coordinate = gp.eventH.checkEvent(this, false);
 
         if (coordinate[0] > 0 && coordinate[1] > 0 && !(initialCol == initialObjCol && initialRow == initialObjRow)) {
-            Entity arrow = new OBJ_Arrow(gp);
+            Entity arrow = new OBJ_Arrow(gp, direction);
             switch (direction) {
                 case "up":
                     arrow.worldX = coordinate[0] * gp.tileSize;
@@ -107,11 +107,11 @@ public class Projectile extends Entity {
                     arrow.worldY = coordinate[1] * gp.tileSize;
                     break;
             }
-
             arrow.direction = direction;
             gp.obj.add(arrow);
             gp.player.pickUpArrow(arrow);
             alive = false;
+            gp.player.saveState();
         }
 
         collisionOn = false;
@@ -135,14 +135,15 @@ public class Projectile extends Entity {
                     worldX += speed;
                     break;
             }
-        } else if (collisionOn == true) {
-            Entity arrow = new OBJ_Arrow(gp);
+        } else if (collisionOn == true){
+            Entity arrow = new OBJ_Arrow(gp, direction);
             arrow.worldX = worldX;
             arrow.worldY = worldY;
             arrow.direction = direction;
             gp.obj.add(arrow);
             gp.player.pickUpArrow(arrow);
             alive = false;
+            gp.player.saveState();
         }
     }
 }
