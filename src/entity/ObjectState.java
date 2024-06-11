84a5b267e52805dcf2eec45c9da0d6d8ff9da42d
KawakitaTanaka <145382809+KawakitaTanaka@users.noreply.github.com>
2024-06-11 16:31:08 +0800

    Add files via upload

---
+++ src/entity/ObjectState.java
@@ -0,0 +1,17 @@
+package entity;
+
+public class ObjectState {
+    public int worldX, worldY;
+    public String type;
+    public String direction; 
+
+    public ObjectState(int worldX, int worldY, String type, String direction) {
+        this.worldX = worldX;
+        this.worldY = worldY;
+        this.type = type;
+        this.direction = direction; 
+    }
+}
+
+
+
---
+++ src/entity/ProjectileState.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class ProjectileState {
+    public boolean alive;
+
+    public ProjectileState(boolean alive) {
+        this.alive = alive;
+    }
+}
